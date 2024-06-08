422e0bf01662e9440edcf439debff344184d3e11
tamer <tammer.ghattas@gmail.com>
2024-06-09 00:42:28 +0300

    Refactor distillation.py to include DataParallel for teacher and student models

--- evals/distillation.py
+++ evals/distillation.py
@@ -2,6 +2,7 @@ import os
 from typing import Union
 import torch
 import torch.nn as nn
+from torch.nn import DataParallel
 from transformers import AutoModelForCausalLM, AutoTokenizer, AutoConfig, DataCollatorForLanguageModeling
 from datasets import load_dataset
 from torch.utils.data import DataLoader
@@ -204,6 +205,7 @@ def train(limit: int = 1000, batch_size: int = 4, max_length: int = 128, epochs:
     assert not (load_chkpt and load_hf_model), "Both load_chkpt and load_hf_model cannot be True at the same time"
     device = f'cuda{f":{gpu}" if gpu else ""}'
     teacher_model.to(device)
+    teacher_model = DataParallel(teacher_model)
     teacher_model.eval()
     if load_hf_model:
         if not is_mamba:
@@ -215,7 +217,7 @@ def train(limit: int = 1000, batch_size: int = 4, max_length: int = 128, epochs:
             student_model = AutoModelForCausalLM.from_config(sanity_student_config).to(device)
         else:
             student_model = get_mamba_model(gpu=gpu)
-
+    student_model = DataParallel(student_model)
     student_model.train()
     optimizer = torch.optim.Adam(student_model.parameters(), lr=learning_rate)
     distill_knowledge(teacher_model, student_model, optimizer, batch_size, max_length, limit=limit, epochs=epochs,
