7b3769f7ff76b921ce6c529f4da9c0652133b071
tamer <tammer.ghattas@gmail.com>
2024-06-25 21:30:42 +0300

    Refactor distillation.py to save student model checkpoint only from the main process and evaluate teacher model

--- evals/distributed_distillation.py
+++ evals/distributed_distillation.py
@@ -276,10 +276,12 @@ def distill_knowledge(teacher_model: AutoModelForCausalLM, student_model: Union[
         
         if os.path.exists("./checkpoints") == False:
             os.mkdir("./checkpoints")
-        torch.save(student_model.state_dict(), f"./checkpoints/student_chkpt_epoch_{epoch}_type_{'mamba' if isinstance(student_model, MambaLMHeadModel) else 'transformer'}_max_length_{max_length}.pt")
+        if accelerator.is_main_process:
+            torch.save(student_model.state_dict(), f"./checkpoints/student_chkpt_epoch_{epoch}_type_{'mamba' if isinstance(student_model, MambaLMHeadModel) else 'transformer'}_max_length_{max_length}.pt")
     
-    # evaluate the teacher model
-    evaluate(teacher_model, is_student=False)
+    if accelerator.is_main_process:
+        # evaluate the teacher model
+        evaluate(teacher_model, is_student=False)
 
 
         
