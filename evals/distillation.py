4c647754748d79c210c970dcd24d30c43363535d
tamer <tammer.ghattas@gmail.com>
2024-07-08 15:26:48 +0300

    Refactor distillation.py to update teacher model path to Pythia model

--- evals/distillation.py
+++ evals/distillation.py
@@ -32,7 +32,8 @@ accelerator = None
 hf_mamba_path = "state-spaces/mamba-790m-hf"
 teacher_model_path = "meta-llama/Meta-Llama-3-8B"
 tiny_model_path = "TinyLlama/TinyLlama-1.1B-Chat-v1.0"
-teacher_model_path = tiny_model_path 
+pythia_model_path = "EleutherAI/pythia-1b"
+teacher_model_path = pythia_model_path 
 
 # teacher_model_path = "mistralai/Mistral-7B-v0.3"
 
