f9a44243d6a28f8a7400affda39829c0b9aab41f
tamer <tammer.ghattas@gmail.com>
2024-07-11 15:28:01 +0300

    Refactor distillation.py to fix typo in evaluate argument

--- evals/distillation.py
+++ evals/distillation.py
@@ -484,7 +484,7 @@ if __name__ == "__main__":
     parser.add_argument("--minimize_dataset", action="store_true", default=False)
     parser.add_argument("--alpha", type=float, default=0.5)
     parser.add_argument("--temperature", type=float, default=2.0)
-    parser.add_argument("--evaulate", action="store_true", default=False)
+    parser.add_argument("--evaluate", action="store_true", default=False)
 
     args = parser.parse_args()
     
