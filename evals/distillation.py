b0f7ec43cddc52c4dfd265285f7ff072b44e9caa
tamer <tammer.ghattas@gmail.com>
2024-07-22 19:32:14 +0300

    Refactor distillation.py to include tf32 option in teacher and student model fine-tuning

--- evals/distillation.py
+++ evals/distillation.py
@@ -335,7 +335,7 @@ def distill_knowledge(teacher_model: AutoModelForCausalLM, student_model: Union[
         evaluate(student_model, eval_dataloader=eval_dataloader, is_student=True, pad_token_id=pad_token_id, gpu=gpu)
 
 
-def finetune_teacher(unique_id: str, batch_size: int, max_length: int, minimize_dataset:bool, epochs:int, lr: float, optimizer: str, mixed_precision: bool, teacher_model_path: str = teacher_model_path):
+def finetune_teacher(unique_id: str, batch_size: int, max_length: int, minimize_dataset:bool, epochs:int, lr: float, optimizer: str, mixed_precision: bool, tf32: bool, teacher_model_path: str = teacher_model_path):
     # fine tune teacher model using hf trainer
 
     train_dataset, _, teacher_data_collator = init_dataloader(batch_size, max_length, "train", minimize_dataset=minimize_dataset, return_dataloader=False)
@@ -384,7 +384,7 @@ def finetune_teacher(unique_id: str, batch_size: int, max_length: int, minimize_
 
 def hf_train(unique_id: str, teacher_model: AutoModelForCausalLM, student_model: Union[MambaLMHeadModel, AutoModelForCausalLM], minimize_dataset: bool,
                 batch_size: int, max_length: int, epochs: int, model_path: str, accumulation_steps: int, alpha: float, temperature: float,
-                  learning_rate: float, mixed_precision: bool, optimizer: str):
+                  learning_rate: float, mixed_precision: bool, optimizer: str, tf32: bool):
     train_dataset, _, teacher_data_collator = init_dataloader(batch_size, max_length, "train", minimize_dataset=minimize_dataset, return_dataloader=False)
     test_dataset, _, _ = init_dataloader(batch_size, max_length, "test", minimize_dataset=minimize_dataset, return_dataloader=False)
     name = f"u{unique_id}_hf_trained_student_{epochs}_epochs_{model_path}_optim{optimizer}_mp{mixed_precision}".replace('.','').replace('/','')
@@ -405,6 +405,7 @@ def hf_train(unique_id: str, teacher_model: AutoModelForCausalLM, student_model:
         optim="adamw_hf",
         gradient_checkpointing=True,
         fp16=mixed_precision,
+        tf32=tf32,
         run_name=name,
     )
     trainer = KDTrainer(
@@ -437,7 +438,7 @@ def train(limit: int = 1000, batch_size: int = 4, max_length: int = 128, epochs:
              is_mamba: bool=False, gpu: int = None, accumulation_steps: int = 1, use_modified_tokenizer: bool = False,
                use_teacher_tokenizer: bool = False, teacher_model_path: str = teacher_model_path,
                  minimize_dataset: bool = False, unique_id: str = '', alpha: float = 0.5, temperature: float = 2.0, hf_trainer: bool = False,
-                   optimizer=None, mixed_precision: bool = False):   
+                   optimizer=None, mixed_precision: bool = False, tf32: bool = False):   
     # assert that if either load_chkpt or load_hf_model is True but not both
     assert not (load_chkpt and load_hf_model), "Both load_chkpt and load_hf_model cannot be True at the same time"
     device = f'cuda{f":{gpu}" if gpu else ""}' if torch.cuda.is_available() else 'mps'
@@ -599,6 +600,7 @@ if __name__ == "__main__":
     parser.add_argument("--hf_trainer", action="store_true", default=False)
     parser.add_argument("--optimizer", type=str, default="adamw_hf")
     parser.add_argument("--mixed_precision", action="store_true", default=False)
+    parser.add_argument("--tf32", action="store_true", default=False)
     args = parser.parse_args()
     
 
@@ -614,6 +616,7 @@ if __name__ == "__main__":
                 "is_mamba": str(args.is_mamba),
                 "accumulation_steps": str(args.accumulation_steps),
                 "mixed_precision": str(args.mixed_precision),
+                "tf32": str(args.tf32),
                 
         }
     if args.use_accelerate:
@@ -636,14 +639,14 @@ if __name__ == "__main__":
         init_logger(wandb)
 
     if args.finetune_teacher:
-        finetune_teacher(unique_id=name_prefix, batch_size=args.batch_size, max_length=args.max_length, minimize_dataset=args.minimize_dataset, epochs=args.epochs, lr=args.learning_rate, optimizer=args.optimizer, teacher_model_path=args.teacher_model_path, mixed_precision=args.mixed_precision)
+        finetune_teacher(unique_id=name_prefix, batch_size=args.batch_size, max_length=args.max_length, minimize_dataset=args.minimize_dataset, epochs=args.epochs, lr=args.learning_rate, optimizer=args.optimizer, teacher_model_path=args.teacher_model_path, mixed_precision=args.mixed_precision, tf32=args.tf32)
     else:
         train(limit=args.limit, batch_size=args.batch_size, max_length=args.max_length, epochs=args.epochs,
             learning_rate=args.learning_rate, load_chkpt=args.load_chkpt, load_hf_model=args.load_hf_model,
             model_path=args.model_path, is_mamba=args.is_mamba, gpu=args.gpu, accumulation_steps=args.accumulation_steps,
             use_modified_tokenizer=args.use_modified_tokenizer, use_teacher_tokenizer=args.use_teacher_tokenizer,
             teacher_model_path=teacher_model_path, minimize_dataset=args.minimize_dataset, unique_id=name_prefix, alpha=args.alpha,
-              temperature=args.temperature, hf_trainer=args.hf_trainer, optimizer=args.optimizer, mixed_precision=args.mixed_precision)
+              temperature=args.temperature, hf_trainer=args.hf_trainer, optimizer=args.optimizer, mixed_precision=args.mixed_precision, tf32=args.tf32)
 
     # example command line run:
     # python evals/distillation.py --limit 1000000000000 --batch_size 8 --max_length 128 --epochs 3 --learning_rate 1e-3 --load_hf_model --model_path /cs/labs/roys/w552295/bamba/full_trained_epoch_2_lr_0.001_is_mamba_True_max_length_128  --is_mamba
